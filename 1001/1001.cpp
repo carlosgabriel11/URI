aa0b6ea160ccb8b95e96f434e06635bdb22c64e9
carlosgabriel11 <cgaa1103@gmail.com>
2018-09-21 11:35:52 -0300

    done the problem 1001

---
+++ 1001/1001.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+//start of the main function
+int main(){
+    //the input numbers
+    int number1;
+    int number2;
+
+    //getting the numbers
+    cin >> number1 >> number2;
+
+    //show the output in the screen
+    cout << "X = " << number1+number2 << endl;
+
+    return 0;
+}
